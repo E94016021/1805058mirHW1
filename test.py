98deacf50fb39d90fadc4682dcdb1df745f6c4e4
LEEE <e94016021@gmail.com>
2018-05-08 06:16:30 +0800

    add ouput txt files

--- test.py
+++ test.py
@@ -1,20 +1,40 @@
 import librosa
 import os
+from dataLoader import Data
 
-rootPath =os.path.abspath('..')
-audioPath = rootPath+"/MIRdata/genres"
-textPath = rootPath+"/MIRdata/gtzan_key-master/gtzan_key/genres"
+rootPath = os.path.abspath('..')
+audioPath = rootPath + "/MIRdata/genres"
+textPath = rootPath + "/MIRdata/gtzan_key-master/gtzan_key/genres"
 
-genre = ['pop','blues','metal','rock','hiphop']
+genre = ['pop', 'blues', 'metal', 'rock', 'hiphop']
 
 # print(os.path.abspath('.'))
 # print(os.path.abspath('..'))
 # print(audioPath)
 # print(textPath)
 
-for x in genre:
-    audio = os.listdir(os.path.join(audioPath, x))
-    text = os.listdir(os.path.join(textPath, x))
-    audio = sorted(audio)
-    print(audio)
+# with open('file.txt', 'w') as f:
+#     print('hello world', file=f)
 
+
+f = open(genre[0]+'.txt', 'w')
+print('fuck u u u u u ', file=f)
+f.close()
+print("gg")
+
+# d = Data('pop')
+# z = d.data[0][0]
+# print(str(z).strip(".au"))
+# z1 = d.data[1][0]
+# print("%s   %s" % (str(z), audioPath))
+# print(z1)
+# k = d[0]
+# print(k)
+# # k = d.__getitem__()
+# print(str(os.path.splitext(d.__getitem__(0))))
+
+# for x in genre:
+#     audio = os.listdir(os.path.join(audioPath, x))
+#     text = os.listdir(os.path.join(textPath, x))
+#     audio = sorted(audio)
+#     print(audio)
